aec2bbd76f9e9cfc372ac4c3941b2a093cabe8cc
djcambas <djcambas@gmail.com>
2024-12-14 20:51:42 -0500

    Passing data through protected routtes now

--- app/hooks/useUser.ts
+++ app/hooks/useUser.ts
@@ -10,7 +10,7 @@ export function useUser(): User | undefined {
   console.log("Matches:", matches); // Debugging line
 
   const parentMatch = matches.find(
-    (match) => match.id === "routes/protected/index"
+    (match) => match.id === "routes/protected"
   );
 
   return (parentMatch?.data as RouteData | undefined)?.user;
--- app/routes.ts
+++ app/routes.ts
@@ -2,12 +2,18 @@ import {
   //type RouteConfig,
   route,
   index,
-  // layout,
-  // prefix,
+  layout,
+  prefix,
 } from "@react-router/dev/routes";
 
 export default [
   index("./routes/_index.tsx"),
   route("/auth/login", "./routes/auth.login.tsx"),
+  ...prefix("/protected", [
+    layout("./routes/protected.tsx", [
+      route("/dashboard", "./routes/protected.dashboard.tsx"),
+      route("/profile", "./routes/protected.profile.tsx"),
+    ]),
+  ]),
 ];
 
